8a1fd8f30c6eb49c28916b3e97cd9987f1cc2860
Mahmut Sat <mmahmutsat@gmail.com>
2020-06-01 02:47:56 +0300

    - Project updated.

--- src/main/java/com/mahmutsat/aop/Inceptors.java
+++ src/main/java/com/mahmutsat/aop/Inceptors.java
@@ -8,22 +8,22 @@ import org.aspectj.lang.annotation.*;
 public class Inceptors {
     @Before("execution(* com.mahmutsat.model.Staff.greeting(..))")
     public void before(JoinPoint joinPoint){
-        System.out.println("\u001b[35m **** Before **** : " + joinPoint.getSignature().getName() + "\u001b[32m");
+        System.out.println("\u001b[35m **** Before **** : " + joinPoint.getSignature().getName() + "\u001b[0m");
     }
 
     @After("execution(* com.mahmutsat.model.Staff.greeting(..))")
     public void after(JoinPoint joinPoint){
-        System.out.println("\u001b[33m *----- After -----* : " + joinPoint.getSignature().getName() + "\u001b[32m");
+        System.out.println("\u001b[33m *----- After -----* : " + joinPoint.getSignature().getName() + "\u001b[0m");
     }
 
     @AfterReturning("execution(* com.mahmutsat.model.Staff.greeting(..))")
     public void afterReturning(JoinPoint joinPoint){
-        System.out.println("\u001b[32m *----- After Returning -----* : " + joinPoint.getSignature().getName() + "\u001b[32m");
+        System.out.println("\u001b[32m *----- After Returning -----* : " + joinPoint.getSignature().getName() + "\u001b[0m");
     }
 
     @Around("execution(* com.mahmutsat.model.Staff.greeting(..))")
     public void around(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("\u001b[31m *----- Around (Before) -----* \u001b[32m");
+        System.out.println("\u001b[31m *----- Around (Before) -----* \u001b[0m");
         joinPoint.proceed();
         System.out.println("\u001b[31m *----- Around (After) -----* \u001b[0m");
     }
